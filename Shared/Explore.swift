bcdd8d614ba7304d16ee31a6e685d32731417669
Andrew Monshizadeh <amonshiz@gmail.com>
2020-08-04 09:46:45 -0500

    Add basic tab files

---
+++ Shared/Explore.swift
@@ -0,0 +1,20 @@
+//
+//  Explore.swift
+//  github-mobile
+//
+//  Created by Andrew Monshizadeh on 8/4/20.
+//
+
+import SwiftUI
+
+struct Explore: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Explore_Previews: PreviewProvider {
+    static var previews: some View {
+        Explore()
+    }
+}
---
+++ Shared/Home.swift
@@ -0,0 +1,20 @@
+//
+//  Home.swift
+//  github-mobile
+//
+//  Created by Andrew Monshizadeh on 8/4/20.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
