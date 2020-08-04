9bacb9bab1a27d9b13af41a34c3181c229f65724
Andrew Monshizadeh <amonshiz@gmail.com>
2020-08-04 10:02:43 -0500

    Setup tabs

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,13 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!").padding()
+  @State var selectedTab: String = "Home"
+  var body: some View {
+    TabView(selection: $selectedTab) {
+      Home()
+        .tabItem {
+          Text("Home")
+          Image(systemName: selectedTab == "Home" ? "house.fill" : "house")
+        }
+        .tag("Home")
+      Notifications()
+        .tabItem {
+          Text("Notifications")
+          Image(systemName: selectedTab == "Notifications" ? "bell.fill" : "bell")
+        }
+        .tag("Notifications")
+      Explore()
+        .tabItem {
+          Text("Explore")
+          Image(systemName: "magnifyingglass")
+        }
+        .tag("Explore")
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
--- Shared/github_mobileApp.swift
+++ Shared/github_mobileApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct github_mobileApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
