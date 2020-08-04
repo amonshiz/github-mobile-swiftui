310cb152f9b0ce13c1887010f469f05458b1daa5
Andrew Monshizadeh <amonshiz@gmail.com>
2020-08-04 09:38:09 -0500

    Initial Commit

---
+++ Shared/github_mobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_mobileApp.swift
+//  Shared
+//
+//  Created by Andrew Monshizadeh on 8/4/20.
+//
+
+import SwiftUI
+
+@main
+struct github_mobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
