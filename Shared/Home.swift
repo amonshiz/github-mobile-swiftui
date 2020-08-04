764bc089480583e0f18a6d349a199c3da5df554c
Andrew Monshizadeh <amonshiz@gmail.com>
2020-08-04 10:37:29 -0500

    Formatting of section headings

--- Shared/Home.swift
+++ Shared/Home.swift
@@ -7,60 +7,93 @@
 
 import SwiftUI
 
+struct SectionTitleModifier: ViewModifier {
+  func body(content: Content) -> some View {
+    content
+      .padding([.top], 15)
+      .padding([.leading], -20)
+      .textCase(.none)
+      .font(.title3)
+      .foregroundColor(.black)
+  }
+}
+
+struct EditButtonModifier: ViewModifier {
+  func body(content: Content) -> some View {
+    content
+      .padding([.trailing], -20)
+      .textCase(.none)
+      .font(.subheadline)
+      .foregroundColor(.blue)
+  }
+}
+
+struct TempView: View {
+  var body: some View {
+    List {
+      Section(
+        header:
+          Text("My Work")
+          .modifier(SectionTitleModifier())
+          .padding([.bottom], 5)
+      ) {
+        ForEach(0..<5) { val in
+          Text("\(val)")
+        }
+      }
+
+      Section(
+        header:
+          HStack(alignment: .bottom) {
+            Text("Favorites")
+              .modifier(SectionTitleModifier())
+            Spacer()
+            EditButton()
+              .modifier(EditButtonModifier())
+          }
+          .padding([.bottom], 5)
+      ) {
+        ForEach(0..<5) { val in
+          Text("\(val)")
+        }
+      }
+
+      Section(
+        header:
+          Text("Recent")
+          .padding([.bottom], 5)
+          .modifier(SectionTitleModifier())
+      ) {
+        ForEach(0..<5) { val in
+          Text("\(val)")
+        }
+      }
+    }
+  }
+}
+
 struct Home: View {
   var body: some View {
     NavigationView {
-      List {
-        Section(
-          header:
-            Text("My Work")
-        ) {
-          ForEach(0..<5) { val in
-            Text("\(val)")
+      TempView()
+        .listStyle(InsetGroupedListStyle())
+        .navigationTitle("Home")
+        .toolbar(items: {
+          ToolbarItem(placement: .destructiveAction) {
+            Circle()
+              .fill(Color.red)
+              .frame(minWidth: 24, minHeight: 24)
           }
-        }
-
-        Section(
-          header:
-            HStack {
-              Text("Favorites")
-              Spacer()
-              EditButton()
-                .foregroundColor(.blue)
+          ToolbarItem(placement: .confirmationAction) {
+            Label {
+              Text("Add")
+            } icon: {
+              Image(systemName: "plus.circle")
             }
-        ) {
-          ForEach(0..<5) { val in
-            Text("\(val)")
+            .foregroundColor(.blue)
           }
-        }
-
-        Section(
-          header:
-            Text("Recent")
-        ) {
-          ForEach(0..<5) { val in
-            Text("\(val)")
-          }
-        }
-      }
-      .listStyle(InsetGroupedListStyle())
-      .navigationTitle("Home")
-      .toolbar(items: {
-        ToolbarItem(placement: .destructiveAction) {
-          Circle()
-            .fill(Color.red)
-            .frame(minWidth: 24, minHeight: 24)
-        }
-        ToolbarItem(placement: .confirmationAction) {
-          Label {
-            Text("Add")
-          } icon: {
-            Image(systemName: "plus.circle")
-          }
-          .foregroundColor(.blue)
-        }
-      })
-      .navigationBarTitleDisplayMode(.inline)
+        })
+        .navigationBarTitleDisplayMode(.inline)
     }
   }
 }
